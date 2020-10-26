3500fa87c0d99ce2502c73877ee49eb6b529e18d
Nils Hollmer <n.hollmer@gmail.com>
2020-10-26 20:07:24 +0100

    Include shared imports in shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent,
-         FooterComponent } from './layout';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+
+import { HeaderComponent, FooterComponent } from './layout';
 
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    NgbModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule
   ],
   exports: [
+      CommonModule,
+      NgbModule,
+      FormsModule,
+      HttpClientModule,
+      RouterModule
   ]
 
 })
