72e72127e5c534b3ffa58027541c970edad00ab9
Nils Hollmer <n.hollmer@gmail.com>
2020-10-29 14:37:32 +0100

    Add functionality to market component, aswell as routing and authguard

--- src/app/market/market.component.ts
+++ src/app/market/market.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { UserService, DatastreamService, StockService } from '@app/core';
 
 @Component({
   selector: 'app-market',
@@ -7,9 +11,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarketComponent implements OnInit {
 
-  constructor() { }
+    currentGold: number;
 
-  ngOnInit(): void {
-  }
+    currencies = {
+        bells: 0,
+        rings: 0,
+        pokedollar: 0,
+        rupees: 0,
+    }
 
+    constructor(
+        private userService : UserService,
+        private dsService: DatastreamService,
+        private stockService: StockService,
+        private router: Router
+
+    ) { }
+
+    ngOnInit() {
+        this.listenForUpdates();
+        this.updateCurrentGold();
+    }
+
+    listenForUpdates() {
+        this.dsService.listen('valueupdate').subscribe(res => {
+            this.updateValues(res);
+        })
+    }
+
+    updateValues(values) {
+        Object.keys(this.currencies).forEach(currency => {
+            this.currencies[currency] = Math.round(values[currency])
+        })
+    }
+
+    buyCurrency(currency, value) {
+        this.stockService.buyCurrency(currency, value)
+        this.router.navigateByUrl('/market')
+        this.currentGold -= value;
+    }
+
+    updateCurrentGold() {
+        this.currentGold = this.userService.getCurrentUser().balance;
+    }
 }
