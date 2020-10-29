3e5b99ec8e216db526d0e3229705862c1a6e7417
Nils Hollmer <n.hollmer@gmail.com>
2020-10-29 14:35:02 +0100

    Add functionality to account page

--- src/app/account/account-routing.module.ts
+++ src/app/account/account-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { AuthGuard } from '@app/core';
+import { AuthGuard } from '@app/core';
 import { AccountComponent } from './account.component';
 
 const routes: Routes = [
     {
         path: '',
         component: AccountComponent,
+        canActivate: [AuthGuard]
     }
 ];
 
--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User, UserService, StockService, DatastreamService } from '@app/core';
 
 @Component({
   selector: 'app-account',
@@ -6,10 +8,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
+    user: User = {} as User;
+    stock: any;
+    @Input()
+    addGold: number = 1;
+    currencies = {
+        bells: 0,
+        rings: 0,
+        pokedollar: 0,
+        rupees: 0,
+    }
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private userService : UserService,
+        private stockService : StockService,
+        private router: Router,
+        private dsService: DatastreamService
+    ) {}
 
+    ngOnInit(): void {
+        this.listenForUpdates();
+        this.updateUserData();
+    }
+
+    updateUserData() {
+        Object.assign(this.user, this.userService.getCurrentUser());
+        this.stockService.getUserStock(this.user.username)
+            .subscribe(data => {
+                this.stock = data["data"]
+            })
+    }
+
+    logout() {
+        this.userService.destroyAuth();
+        this.router.navigateByUrl('/');
+    }
+
+    addBalance() {
+        this.userService.editBalance("add", this.addGold);
+        this.user.balance += this.addGold;
+    }
+
+    listenForUpdates() {
+        this.dsService.listen('valueupdate').subscribe(res => {
+            this.updateValues(res);
+        })
+    }
+
+    updateValues(values) {
+        Object.keys(this.currencies).forEach(currency => {
+            this.currencies[currency] = Math.round(values[currency])
+        })
+    }
+
+    sellCurrency(id, currency, value) {
+        this.stockService.sellCurrency(id, currency, value);
+        this.router.navigateByUrl('/account')
+        this.updateUserData();
+        this.user.balance += value;
+    }
 }
